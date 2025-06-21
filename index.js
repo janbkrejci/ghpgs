e24cab8133d983bf0bf7a8aac8c1d4a4e13aa135
Jan B. Krejčí <janbkrejci@gmail.com>
2025-06-21 21:16:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const pipe = f => g => x => f(g(x));
+const compose = f => g => x => f(g(x));
 
 const setText = el => txt => {
     el.innerText = txt;
 }
 const out = setText(document.getElementById("out"));
 const stringify = x => JSON.stringify(x, null, 2);
-const strOut = pipe(stringify)(out);
+const strOut = compose(out)(stringify);
 
 const x = {
     a: 42,
